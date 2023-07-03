eaa450098ebc2a45fb1da69108fe9060690c121d
aleefa <116252564+aleefa@users.noreply.github.com>
2023-07-03 15:44:57 +0530

    Add files via upload

---
+++ rename.sh
@@ -0,0 +1,15 @@
+#shell script to rename all ordinary files in a directory with filename.PID of shell
+if [ $# -ne 0 ]
+then
+echo "synatx error"
+exit
+fi
+for file in *
+do
+if [ -f $file ]
+then
+newname="$file.$$"
+mv $file $newname
+fi
+done
+exit
