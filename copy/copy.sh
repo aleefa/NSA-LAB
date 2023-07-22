f55554b1065efa32768d51104605c537f3926438
aleefa <116252564+aleefa@users.noreply.github.com>
2023-07-22 12:37:36 +0530

    Add files via upload

---
+++ copy/copy.sh
@@ -0,0 +1,28 @@
+#copy set of files as pairs
+if [ $# -lt 2 ]
+then
+echo "Syntax Error.\nSyntax --> $0 [filename] [filename] ......"
+exit
+fi
+r=`expr $# % 2`
+if [ $r -ne 0 ]
+then
+echo "File names are not in pairs"
+exit 1
+fi
+while [ $# -ne 0 ]
+do
+if [ -f $1 -a -f $2 ]
+then
+cp $1 $2
+echo "$1 and $2 copied."
+shift
+shift
+else
+echo "$1 and $2 copying failed (File(s) do not exist) "
+exit 1
+fi
+done
+echo "Copying Completed"
+exit 0
+
